a62b880b1699a5413c8ce3d8b0ed0d9e63b4f9af
Saul Florez <sflorez@distritohosting.com>
2018-10-05 15:09:07 -0400

    Purchases

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -62,11 +62,11 @@
             <img src="{{ asset('img/icons/ico-pedidos.png') }}" alt="" class="img-responsive">Pedidos
         </a> 
     </li>
-    <li>
+    {{-- <li>
         <a href="{{ url('/admin/clients') }}" class="collapsible-label1 tooltipped" data-position="right" data-delay="50" data-tooltip="Aliados">
             <img src="{{ asset('img/icons/ico-clients.png') }}" alt="" class="img-responsive">Pedidos
         </a> 
-    </li>
+    </li> --}}
     <li>
         <a href="{{ url('/admin/blogs') }}" class="collapsible-label1 tooltipped" data-position="right" data-delay="50" data-tooltip="Blogs">
             <img src="{{ asset('img/icons/ico-blogs.png') }}" alt="" class="img-responsive">Blogs
