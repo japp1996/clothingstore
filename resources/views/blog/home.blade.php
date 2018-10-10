fb8fe414e78d7e86014c4e681241e722c521134b
omarj94 <oblanco@distritohosting.com>
2018-10-10 09:50:27 -0400

    U

--- resources/views/blog/home.blade.php
+++ resources/views/blog/home.blade.php
@@ -10,16 +10,18 @@
 		<p>@lang('Page.Blog.SubTitle')</p>
 		<div class="masonry">
 			@foreach($blogs as $blog)
-				<a href="{{ URL('mundo/view',$blog->id) }}">
-					<div class="item">
-						<img src="{{ URL('img/blogs/'.$blog->images[0]->file) }}" />
-						<div class="container-text">
-							<h4>{{ \App::getLocale() == 'es' ? $blog->title : $blog->title_english }}</h4>
-							<p class="fecha">@lang('Page.Blog.Fecha'): {{ \Carbon\Carbon::parse($blog->created_at)->format('d/m/Y') }}</p>
-							<p class="ellipsis">{!! \App::getLocale() == 'es' ? nl2br($blog->description) : nl2br($blog->description_english) !!}</p>
+				<div class="item-column">
+					<a href="{{ URL('mundo/view',$blog->id) }}">
+						<div class="item">
+							<img src="{{ URL('img/blogs/'.$blog->images[0]->file) }}" />
+							<div class="container-text">
+								<h4>{{ \App::getLocale() == 'es' ? $blog->title : $blog->title_english }}</h4>
+								<p class="fecha">@lang('Page.Blog.Fecha'): {{ \Carbon\Carbon::parse($blog->created_at)->format('d/m/Y') }}</p>
+								<p class="ellipsis">{!! \App::getLocale() == 'es' ? nl2br($blog->description) : nl2br($blog->description_english) !!}</p>
+							</div>
 						</div>
-					</div>
-				</a>
+					</a>
+				</div>
 			@endforeach
 		</div>
 		<div class="text-center">
@@ -32,10 +34,10 @@
 	<script type="text/javascript">
 		$(document).ready(function() {
 			if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
-				$('.item').css('display','inline-block');
+				$('.item-column').css('display','inline-block');
 			}
 
-			$('.item').animate({
+			$('.item-column').animate({
 				opacity: '1'
 			},0);
 		});
